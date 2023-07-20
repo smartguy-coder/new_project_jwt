697b9c824e3df4e8bea2d5de8573266291417c86
Василь Картичак <w.i.k.mailua@gmail.com>
2023-07-20 19:40:22 +0300

    intro JWT

--- jwt_theory.py
+++ jwt_theory.py
@@ -1,10 +1,27 @@
-print(1)
-print(2)
+import jwt
+import datetime
+import time
 
+payload = {
+    'my_name': 'Vasyl',
+    'password': 'kdfjhgkl;dfjkgdfkluhg;.dfjglkdfgkdfli;ghdfljgkljdfgkl;dfg',
 
+    'exp': datetime.datetime.utcnow() + datetime.timedelta(seconds=90),
+}
 
-print(555)
+encode_jwt = jwt.encode(
+    payload=payload,
+    key='secret',
+    algorithm='HS256',
+)
+time.sleep(3)
+decoded = jwt.decode(
+    encode_jwt,
+    'secret',
+    algorithms=['HS256'],
+    # options={
+    #     'verify_signature': False,
+    # }
+)
 
-
-print(88888888)
-
+print(decoded)
