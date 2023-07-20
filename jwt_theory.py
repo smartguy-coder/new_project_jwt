83e1d315928f6e876a8dcd9bdf8e2e21370712be
Василь Картичак <w.i.k.mailua@gmail.com>
2023-07-20 19:45:50 +0300

    intro JWT - secret

--- jwt_theory.py
+++ jwt_theory.py
@@ -2,6 +2,8 @@ import jwt
 import datetime
 import time
 
+import config
+
 payload = {
     'my_name': 'Vasyl',
     'password': 'kdfjhgkl;dfjkgdfkluhg;.dfjglkdfgkdfli;ghdfljgkljdfgkl;dfg',
@@ -11,13 +13,13 @@ payload = {
 
 encode_jwt = jwt.encode(
     payload=payload,
-    key='secret',
+    key=config.JWT_SECRET,
     algorithm='HS256',
 )
 time.sleep(3)
 decoded = jwt.decode(
     encode_jwt,
-    'secret',
+    config.JWT_SECRET,
     algorithms=['HS256'],
     # options={
     #     'verify_signature': False,
